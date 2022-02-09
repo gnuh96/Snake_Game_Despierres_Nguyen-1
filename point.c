f57f49d92bf21c952f78bdb892647b9574de40e5
gnuh96 <ntuanhung.96@gmail.com>
2022-02-09 11:18:08 +0100

    modifier point.c

--- point.c
+++ point.c
@@ -6,7 +6,18 @@ typedef struct Point{
     int y;
 }Point;
 
-Point deplace(int dx, int dy) {
-    x = x + dx;
-    y = y + dy;
+void deplace(Point *p, int dx, int dy) {
+    p->x = p->x + dx;
+    p->y = p->y + dy;
 }
+
+void affiche(Point *p) {
+    printf("( %d , %d)\n", p->x, p->y);
+}
+
+// int main () {
+//     Point p1 = {1,2};
+//     affiche(&p1);
+//     deplace(&p1, 2, 1);
+//     affiche(&p1);
+// }

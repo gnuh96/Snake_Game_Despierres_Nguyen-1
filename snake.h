c64bafe836e1f3bd825e89669888acb9da858ac0
Trystan Despierres <despierrestry@yahoo.fr>
2022-02-12 00:05:14 +0100

    Ajout du changement de score du snake

--- snake.h
+++ snake.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <point.h>
+#include "point.h"
 
 typedef struct Snake{
     int id;
     int taille;
     Point *body; //liste des cellules ou le Snake occupe
+    int score; //score du snake
 }Snake;
 
 Snake initSnake(int i){
@@ -15,5 +16,25 @@ Snake initSnake(int i){
     s.body=(Point*)malloc(s.taille*sizeof(Point));
     Point p = {1, i+1};
     s.body[0]= p;
+    s.score=0;
 }
 
+void updateScore(char fruit, int *score){
+    switch (fruit){
+
+    case 'F': //Fraise
+        *score+=5;
+        break;
+    case 'C': //Cerises
+        *score+=3;
+        break;
+    case 'B': //Banane
+        *score+=2;
+        break;
+    case 'P': //Pomme
+        *score+=1;
+        break;
+    default:
+        break;
+    }
+}

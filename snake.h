f8732f99299c3ff47b2da80277105343bfe72184
Trystan Despierres <despierrestry@yahoo.fr>
2022-02-12 02:33:15 +0100

    Ajout d'une fonction de deplacement du snake

--- snake.h
+++ snake.h
@@ -54,10 +54,34 @@ void afficheScore(Snake s){
     printf("Snake %c : %d\n", s.id, s.score);
 }
 
-Point deplaceSnake(int hauteur, int largeur){
+Point deplaceTete(int hauteur, int largeur){
     int x, y;
     x=rand()% largeur;
     y=rand()% hauteur;
     Point p={x,y};
     return p;
+}
+
+//A tester
+void deplaceSnake(Plateau plat, Snake s, int hauteur, int largeur){
+    //Deplacement de la tete
+    Point tete=deplaceTete(hauteur, largeur);
+    
+    Point pointTmp1, pointTmp2;
+    pointTmp1=s.body[0]; //pointTmp1 prend la position actuelle de la tete
+    deplace(&s.body[0],tete.x,tete.y);
+
+    //Recuperation du fruit
+    char fruit=getChar(plat, tete.x, tete.y);
+    updateScore(fruit, &s.score);
+
+    //Deplacement du reste du corps
+    if (s.taille>1)
+    {
+        for(int i=1;i<s.taille;i++){
+            pointTmp2=s.body[i]; //pointTmp2 prend la position actuelle du point
+            deplace(&s.body[i], pointTmp1.x, pointTmp1.y); //s.body[i] prend la position du point precedent
+            pointTmp1=pointTmp2; //pointTmp1 prend la position en memoire dans pointTmp2
+        }
+    }
 }
